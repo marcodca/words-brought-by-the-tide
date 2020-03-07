fe2c33fdc84a880fe3252f2d21ee9c960089a097
marco_dca <marcodecara@gmail.com>
2020-03-07 12:22:51 +0100

    Initial commit and test

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import { Counter } from "./features/counter/Counter";
+import "./App.css";
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+  method: "GET",
+  headers: {
+    "x-rapidapi-host": "quotes15.p.rapidapi.com",
+    "x-rapidapi-key": process.env.REACT_APP_X_RAPIDPI_KEY
+  }
+})
+  .then(response => {
+    response.json().then(a => console.log(a));
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 function App() {
   return (
